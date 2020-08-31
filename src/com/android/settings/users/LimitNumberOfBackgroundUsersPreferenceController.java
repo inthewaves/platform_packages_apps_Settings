ac33e59d36c9c2ab8647964fdc499566887b7aab
inthewaves <inthewaves@pm.me>
2020-08-31 11:42:07 -0700

    use != 0 <=> == 1 and conditionally unavailable

--- src/com/android/settings/users/LimitNumberOfBackgroundUsersPreferenceController.java
+++ src/com/android/settings/users/LimitNumberOfBackgroundUsersPreferenceController.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
 package com.android.settings.users;
 
 import android.content.Context;
@@ -51,14 +36,14 @@ public class LimitNumberOfBackgroundUsersPreferenceController extends TogglePref
         if (!mUserCaps.isAdmin()) {
             return DISABLED_FOR_USER;
         } else {
-            return mUserCaps.mUserSwitcherEnabled ? AVAILABLE : DISABLED_FOR_USER;
+            return mUserCaps.mUserSwitcherEnabled ? AVAILABLE : CONDITIONALLY_UNAVAILABLE;
         }
     }
 
     @Override
     public boolean isChecked() {
         return Settings.Global.getInt(mContext.getContentResolver(),
-                Settings.Global.RUNNING_USERS_LIMIT_ENABLED, 1) == 1;
+                Settings.Global.RUNNING_USERS_LIMIT_ENABLED, 1) != 0;
     }
 
     @Override
