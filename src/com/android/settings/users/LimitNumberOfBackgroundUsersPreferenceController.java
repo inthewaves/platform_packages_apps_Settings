681b01160396fa36e9510eeb965a1f6f67ddbe29
inthewaves <inthewaves@pm.me>
2020-08-30 18:45:23 -0700

    rename stuff

--- src/com/android/settings/users/LimitNumberOfBackgroundUsersPreferenceController.java
+++ src/com/android/settings/users/LimitNumberOfBackgroundUsersPreferenceController.java
@@ -19,50 +19,60 @@ import android.content.Context;
 import android.os.UserManager;
 import android.provider.Settings;
 
+import androidx.preference.Preference;
+
 import com.android.settings.R;
 import com.android.settings.core.TogglePreferenceController;
 
 public class LimitNumberOfBackgroundUsersPreferenceController extends TogglePreferenceController {
 
-    private final UserManager mUm;
+    private final UserCapabilities mUserCaps;
 
     public LimitNumberOfBackgroundUsersPreferenceController(Context context, String key) {
         super(context, key);
-        mUm = (UserManager) context.getSystemService(Context.USER_SERVICE);
+        mUserCaps = UserCapabilities.create(context);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        mUserCaps.updateAddUserCapabilities(mContext);
+
+        preference.setSummary(getSummary());
+        if (!isAvailable()) {
+            preference.setVisible(false);
+        } else {
+            preference.setVisible(mUserCaps.mUserSwitcherEnabled);
+        }
     }
 
     @Override
     public int getAvailabilityStatus() {
-        if (!mUm.isAdminUser()) {
+        if (!mUserCaps.isAdmin()) {
             return DISABLED_FOR_USER;
         } else {
-            return AVAILABLE;
+            return mUserCaps.mUserSwitcherEnabled ? AVAILABLE : DISABLED_FOR_USER;
         }
     }
 
     @Override
     public boolean isChecked() {
         return Settings.Global.getInt(mContext.getContentResolver(),
-                Settings.Global.BACKGROUND_USERS_LIMIT_ENABLED, 1) == 1;
+                Settings.Global.RUNNING_USERS_LIMIT_ENABLED, 1) == 1;
     }
 
     @Override
     public boolean setChecked(boolean isChecked) {
         return Settings.Global.putInt(mContext.getContentResolver(),
-                Settings.Global.BACKGROUND_USERS_LIMIT_ENABLED, isChecked ? 1 : 0);
+                Settings.Global.RUNNING_USERS_LIMIT_ENABLED, isChecked ? 1 : 0);
     }
-
 
     @Override
     public CharSequence getSummary() {
-        if (isChecked()) {
-            final int maxRunningUsers = mContext.getResources()
-                    .getInteger(com.android.internal.R.integer.config_multiuserMaxRunningUsers);
-            return mContext.getString(R.string.user_settings_limit_number_background_users_summary_on,
-                    maxRunningUsers);
-        } else {
-            return mContext.getString(R.string.user_settings_limit_number_background_users_summary_off);
-        }
-
+        final int maxRunningUsers = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_multiuserMaxRunningUsers);
+        return mContext.getString(R.string.user_settings_limit_number_background_users_summary,
+                maxRunningUsers);
     }
 }
+
